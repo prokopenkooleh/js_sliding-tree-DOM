c0980efab676d4419e66170cb607608c87ba568d
Oleh Prokopenko <olehprokopenko1@gmail.com>
2025-11-30 11:28:00 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,26 @@
+"use strict";
+const allLi = document.querySelectorAll("li");
+allLi.forEach((li)=>{
+    [
+        ...li.childNodes
+    ].forEach((node)=>{
+        if (node.nodeType === Node.TEXT_NODE && node.textContent.trim() !== "") {
+            const span = document.createElement("span");
+            span.textContent = node.textContent;
+            node.replaceWith(span);
+        }
+    });
+    li.firstElementChild.addEventListener("click", (ev)=>{
+        const targetLi = ev.target.closest("li");
+        ev.stopPropagation();
+        const innerULs = [
+            ...targetLi.children
+        ].filter((child)=>child.tagName === "UL");
+        innerULs.forEach((ul)=>{
+            if (ul.style.display === "none") ul.style.display = "block";
+            else ul.style.display = "none";
+        });
+    });
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
